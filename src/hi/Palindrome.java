b96c260b71049e1e298c374d276e8d0c44bf7cda
umadevi <neelamumadevi978@gmail.com>
2026-06-14 23:19:43 +0530

    added palindrome feature

---
+++ src/hi/Palindrome.java
@@ -0,0 +1,30 @@
+package hi;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	static int reverseNum(int num) {
+		int sum = 0;
+		while (num > 0) {
+			int r = num % 10;
+			sum = (sum * 10) + r;
+			num = num / 10;
+		}
+		return sum;
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int nextInt = sc.nextInt();
+		int revNum = reverseNum(nextInt);
+
+		if (nextInt == revNum) {
+			System.out.println("it is palindrome number");
+		} else {
+			System.out.println("it is not a palindrome  number");
+		}
+
+		sc.close();
+	}
+}
