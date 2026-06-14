5b61f964580e2bc5428e3f5e549ac755b87370cf
umadevi <neelamumadevi978@gmail.com>
2026-06-14 22:25:12 +0530

    oddnumber file is commiting

---
+++ src/hello/OddNumber.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.Scanner;
+
+public class OddNumber {
+	static boolean checkOdd(int num) {
+		return num%2==0;
+	}
+    public static void main(String args[]) {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("enter the number");
+    	int nextInt = sc.nextInt();
+    	boolean checkOdd = checkOdd(nextInt);
+    	if(!checkOdd) {
+    		System.out.println("it is an odd number");
+    	}else {
+    		System.out.println("not an odd number");
+    	}
+    }
+}
