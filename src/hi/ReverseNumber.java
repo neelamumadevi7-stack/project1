b3cdb8316df4f74b39e6dbf40fd70306068bb044
umadevi <neelamumadevi978@gmail.com>
2026-06-14 23:06:29 +0530

    added rev number

---
+++ src/hi/ReverseNumber.java
@@ -0,0 +1,27 @@
+package hi;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	static int reverseNum(int num) {
+		int sum=0;
+		while(num>0) {
+			int r=num%10;
+			sum=(sum*10)+r;
+			num=num/10;
+		}
+		return sum;
+	}
+    public static void main(String args[]) {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("enter the number");
+    	int nextInt = sc.nextInt();
+    	int revNum = reverseNum(nextInt);
+    	System.out.println("rev num is "+revNum);
+		/*
+		 * if(nextInt==revNum) { System.out.println("it is prime number"); }else {
+		 * System.out.println("it is not a prime number"); }
+		 */
+    	sc.close();
+    }
+}
